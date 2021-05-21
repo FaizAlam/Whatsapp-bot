1c00d7d09c0586b820f68d4deb726fa37644918b
FaizAlam <mohdfaizalam53@gmail.com>
2021-05-22 00:04:42 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask,request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+
+@app.route('/sms',methods=['POST'])
+def sms_reply():
+    msg = request.form.get('Body')
+    # Create reply
+    resp = MessagingResponse()
+    resp.message("You said: {}".format(msg))
+
+    return str(resp)
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
